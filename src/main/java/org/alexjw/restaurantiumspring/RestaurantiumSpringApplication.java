e6a2218ae75110f9539f36dff71ed0a2aa7becaf
Alex J <abel.oalex@gmail.com>
2020-07-06 13:58:50 -0400

    initial commit

---
+++ src/main/java/org/alexjw/restaurantiumspring/RestaurantiumSpringApplication.java
@@ -0,0 +1,13 @@
+package org.alexjw.restaurantiumspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantiumSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantiumSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alexjw/restaurantiumspring/RestaurantiumSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alexjw.restaurantiumspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantiumSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
